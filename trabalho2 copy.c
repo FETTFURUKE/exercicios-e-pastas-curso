444d75352611fea537d1aef4b585f65d9bf033a0
FETTFURUKE <augustobernardo105@gmail.com>
2026-05-31 19:18:24 +0000

    31 de mai. de 2026, 16:18

--- trabalho2 copy.c
+++ trabalho2 copy.c
@@ -18,10 +18,13 @@ char menu;
 
 do{
 printf("  SISTEMA DE PEDIDOS  \n");
-Prinf("1 - Cadastro da ordem ");
+Prinf("1 - Gerenciamento de ordens ");
 printf("2 - Listar pedidos");
 printf("3 - Buscar pedido");
-printf("4 - ")
+printf("4 - Status dos pedidos ");
+printf("5 - Maior prioridade");
+printf("6 - Remover pedido")
+printf("7 - Desligar")
 
 
 
