4ca81d9d1e08ff6993caa4ecb2cd7e3fe178dc32
FETTFURUKE <augustobernardo105@gmail.com>
2026-06-01 23:49:19 +0000

    1 de jun. de 2026, 20:49

---
+++ exercicio.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+ 
+int temperatura;
+
+
+int main{
+
+
+if(temperatura <= 10){
+
+
+printf("muito frio \n");
+
+}
+
+else if(temperatura 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
