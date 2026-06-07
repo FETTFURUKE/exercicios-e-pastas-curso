ddc6ad378ddcfeaf5be6e457ea77fd9d7e3794fa
FETTFURUKE <augustobernardo105@gmail.com>
2026-06-07 20:21:54 +0000

    7 de jun. de 2026, 17:21

--- treinandoprova.c
+++ treinandoprova.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 
-typedef struct{
+
 float temperatura;
+int hora, opcao;
+
+
+float Lertemperatura() {
+printf("Digite a temperatura:  \n");
+scanf("%f", &temperatura);
 
 }
 
+int Lerhora() {
+printf("digite a hora do registro: 00:00  \n");
+scanf("%d", &hora);
 
 
+}
 
 int main(){
 
 float temperatura;
+int hora, opcao;
+
+switch( opcao){
+
+case 1:
+
+
+
+
+
+case 2:
+
+
+
+}
 
 
 
